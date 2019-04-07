c2ee356117f1c5ca7c89812fb79f34504ae50d84
Marc André Wittorf <marc-a@gmx.de>
2019-04-07 17:14:32 +0200

    Use new installer structure

--- magisk_module.mk
+++ magisk_module.mk
@@ -1,84 +1,73 @@
 $(eval $(call start_package)) # not really a package, but we may reset stuff just in case
 
-MAGISK_TEMPLATE_VERSION?=17000
+MAGISK_INSTALLER_VERSION?=v19.0
 
 .PHONY: module
 module: $(BUILD_DIR)/magisk_ssh-$(VERSION).zip
 
-$(ARCHIVE_DIR)/magisk_template-$(MAGISK_TEMPLATE_VERSION).tar.gz: $(ARCHIVE_DIR)
-	wget -O $(ARCHIVE_DIR)/magisk_template-$(MAGISK_TEMPLATE_VERSION).tar.gz --no-use-server-timestamps "https://github.com/topjohnwu/magisk-module-template/archive/$(MAGISK_TEMPLATE_VERSION).tar.gz"
-	# No checksum verification, as GitHub may recreate archives which changes its checksum
-
-$(BUILD_DIR)/module/stamp.module-extracted: $(ARCHIVE_DIR)/magisk_template-$(MAGISK_TEMPLATE_VERSION).tar.gz
+$(BUILD_DIR)/module/stamp.module-created:
 	mkdir -p $(BUILD_DIR)/module/magisk_ssh
-	tar -xvf "$(ARCHIVE_DIR)/magisk_template-$(MAGISK_TEMPLATE_VERSION).tar.gz" -C "$(BUILD_DIR)/module/magisk_ssh" --strip 1
+	mkdir -p $(BUILD_DIR)/module/magisk_ssh/arch
+	mkdir -p $(BUILD_DIR)/module/magisk_ssh/common
 	touch $(BUILD_DIR)/module/stamp.module-extracted
 
-$(BUILD_DIR)/module/stamp.module-prop: $(BUILD_DIR)/module/stamp.module-extracted
-	cp $(ROOT_DIR)/module_data/module.prop $(BUILD_DIR)/module/magisk_ssh
-	touch $(BUILD_DIR)/module/stamp.module-prop
+$(BUILD_DIR)/module/stamp.module-staticdata: $(BUILD_DIR)/module/stamp.module-created
+	cp -R $(ROOT_DIR)/module_data/. $(BUILD_DIR)/module/magisk_ssh/
+	touch $(BUILD_DIR)/module/stamp.module-staticdata
 
-$(BUILD_DIR)/module/stamp.module-readme: $(BUILD_DIR)/module/stamp.module-extracted
-	cp $(ROOT_DIR)/module_data/README.md $(BUILD_DIR)/module/magisk_ssh
-	touch $(BUILD_DIR)/module/stamp.module-readme
-
-$(BUILD_DIR)/module/stamp.module-config: $(BUILD_DIR)/module/stamp.module-extracted
-	$(ROOT_DIR)/module_data/modify_config.py $(BUILD_DIR)/module/magisk_ssh/config.sh
-	touch $(BUILD_DIR)/module/stamp.module-config
-
-$(BUILD_DIR)/module/stamp.module-sshdconfig: $(BUILD_DIR)/module/stamp.module-extracted
-	cp $(ROOT_DIR)/module_data/sshd_config $(BUILD_DIR)/module/magisk_ssh/common/
-	touch $(BUILD_DIR)/module/stamp.module-sshdconfig
-
-$(BUILD_DIR)/module/stamp.module-service: $(BUILD_DIR)/module/stamp.module-extracted
-	cp -f $(ROOT_DIR)/module_data/service.sh $(BUILD_DIR)/module/magisk_ssh/common/
-	touch $(BUILD_DIR)/module/stamp.module-service
-
-$(BUILD_DIR)/module/stamp.module-library-wrapper: $(BUILD_DIR)/module/stamp.module-extracted
-	cp -f $(ROOT_DIR)/module_data/magisk_ssh_library_wrapper $(BUILD_DIR)/module/magisk_ssh/common/
-	touch $(BUILD_DIR)/module/stamp.module-library_wrapper
-
-$(BUILD_DIR)/module/stamp.module-binaries: $(BUILD_DIR)/module/stamp.module-extracted \
-                                           $(INSTALLED_FILES_arm)                     \
-                                           $(INSTALLED_FILES_arm64)                   \
-                                           $(INSTALLED_FILES_x86)                     \
-                                           $(INSTALLED_FILES_x86_64)                  \
-                                           $(INSTALLED_FILES_mips)                    \
+$(BUILD_DIR)/module/stamp.module-binaries: $(BUILD_DIR)/module/stamp.module-created \
+                                           $(INSTALLED_FILES_arm)                   \
+                                           $(INSTALLED_FILES_arm64)                 \
+                                           $(INSTALLED_FILES_x86)                   \
+                                           $(INSTALLED_FILES_x86_64)                \
+                                           $(INSTALLED_FILES_mips)                  \
                                            $(INSTALLED_FILES_mips64)
-	cp -r $(BUILD_DIR)/arm/usr    $(BUILD_DIR)/module/magisk_ssh/common/arm
-	cp -r $(BUILD_DIR)/arm64/usr  $(BUILD_DIR)/module/magisk_ssh/common/arm64
-	cp -r $(BUILD_DIR)/x86/usr    $(BUILD_DIR)/module/magisk_ssh/common/x86
-	cp -r $(BUILD_DIR)/x86_64/usr $(BUILD_DIR)/module/magisk_ssh/common/x86_64
-	cp -r $(BUILD_DIR)/mips/usr   $(BUILD_DIR)/module/magisk_ssh/common/mips
-	cp -r $(BUILD_DIR)/mips64/usr $(BUILD_DIR)/module/magisk_ssh/common/mips64
+	cp -r $(BUILD_DIR)/arm/usr    $(BUILD_DIR)/module/magisk_ssh/arch/arm
+	cp -r $(BUILD_DIR)/arm64/usr  $(BUILD_DIR)/module/magisk_ssh/arch/arm64
+	cp -r $(BUILD_DIR)/x86/usr    $(BUILD_DIR)/module/magisk_ssh/arch/x86
+	cp -r $(BUILD_DIR)/x86_64/usr $(BUILD_DIR)/module/magisk_ssh/arch/x86_64
+	cp -r $(BUILD_DIR)/mips/usr   $(BUILD_DIR)/module/magisk_ssh/arch/mips
+	cp -r $(BUILD_DIR)/mips64/usr $(BUILD_DIR)/module/magisk_ssh/arch/mips64
 	touch $(BUILD_DIR)/module/stamp.module-binaries
 
-$(BUILD_DIR)/module/stamp.module-initscript: $(BUILD_DIR)/arm/openssh/stamp.built       \
-                                             $(BUILD_DIR)/module/stamp.module-extracted
+$(BUILD_DIR)/module/stamp.module-initscript: $(BUILD_DIR)/arm/openssh/stamp.built     \
+                                             $(BUILD_DIR)/module/stamp.module-created
 	sed -e 's:#!/bin/sh:#!/system/bin/sh:'         \
 	    -e 's#=/bin#=/system/bin#'                 \
 	    -e 's#.*PidFile.*##'                       \
 	    -e 's#sbin#bin#'                           \
-	    -e 's#^prefix=.*#: $${MODDIR:="$$(realpath "$$(dirname "$$0")")"}\nprefix=\"$$MODDIR/usr\"#' \
+	    -e 's#^prefix=.*#: $${MODDIR:="$$(realpath "$$(dirname "$$0")")"}\nexport MODDIR\nprefix=\"$$MODDIR/usr\"#' \
 	    -e 's#@COMMENT_OUT_RSA1@.*##'              \
 	    $(BUILD_DIR)/arm/openssh/opensshd.init     \
 	    > $(BUILD_DIR)/module/magisk_ssh/common/opensshd.init
 	touch $(BUILD_DIR)/module/stamp.module-initscript
 
-$(BUILD_DIR)/module/stamp.module: $(BUILD_DIR)/module/stamp.module-extracted  \
-                                  $(BUILD_DIR)/module/stamp.module-prop       \
-                                  $(BUILD_DIR)/module/stamp.module-readme     \
-                                  $(BUILD_DIR)/module/stamp.module-config     \
+$(BUILD_DIR)/module/stamp.module: $(BUILD_DIR)/module/stamp.module-staticdata \
                                   $(BUILD_DIR)/module/stamp.module-binaries   \
-                                  $(BUILD_DIR)/module/stamp.module-sshdconfig \
-                                  $(BUILD_DIR)/module/stamp.module-service    \
-                                  $(BUILD_DIR)/module/stamp.module-initscript \
-                                  $(BUILD_DIR)/module/stamp.module-library-wrapper
+                                  $(BUILD_DIR)/module/stamp.module-initscript
 	touch $(BUILD_DIR)/module/stamp.module
 
-$(BUILD_DIR)/magisk_ssh_$(VERSION).zip: $(BUILD_DIR)/module/stamp.module
+$(ARCHIVE_DIR)/magisk_template-$(MAGISK_INSTALLER_VERSION).sh: $(ARCHIVE_DIR)/stamp.created
+	wget -O "$(ARCHIVE_DIR)/magisk_installer-$(MAGISK_INSTALLER_VERSION).sh" \
+	     --no-use-server-timestamps                                          \
+	     "https://raw.githubusercontent.com/topjohnwu/Magisk/$(MAGISK_INSTALLER_VERSION)/scripts/module_installer.sh"
+	cd "$(ARCHIVE_DIR)";                                                                         \
+	sha512sum -c $(ROOT_DIR)/checksums/magisk_installer-$(MAGISK_INSTALLER_VERSION).sh.sha512 || \
+	(mv "$(ARCHIVE_DIR)/magisk_installer-$(MAGISK_INSTALLER_VERSION).sh"                         \
+	    "$(ARCHIVE_DIR)/magisk_installer-$(MAGISK_INSTALLER_VERSION).sh.invalid_checksum";       \
+	 false)
+
+$(BUILD_DIR)/module/stamp.module-standalone: $(BUILD_DIR)/module/stamp.module \
+                                             $(ARCHIVE_DIR)/magisk_template-$(MAGISK_INSTALLER_VERSION).sh
+	cp -r $(BUILD_DIR)/module/magisk_ssh/. $(BUILD_DIR)/module/magisk_ssh_standalone
+	cp "$(ARCHIVE_DIR)/magisk_installer-$(MAGISK_INSTALLER_VERSION).sh" \
+	   "$(BUILD_DIR)/module/magisk_ssh_standalone/META-INF/com/google/android/update-binary"
+	touch $(BUILD_DIR)/module/stamp.module-standalone
+
+$(BUILD_DIR)/magisk_ssh_$(VERSION).zip: $(BUILD_DIR)/module/stamp.module-standalone
 	rm -f $(BUILD_DIR)/magisk_ssh_$(VERSION).zip
-	cd $(BUILD_DIR)/module/magisk_ssh; zip -9 -r $(shell realpath $(BUILD_DIR)/magisk_ssh_$(VERSION).zip) *
+	cd $(BUILD_DIR)/module/magisk_ssh_standalone; \
+	zip -9 -r $(shell realpath $(BUILD_DIR)/magisk_ssh_$(VERSION).zip) *
 
 .PHONY: zip
 zip: $(BUILD_DIR)/magisk_ssh_$(VERSION).zip
