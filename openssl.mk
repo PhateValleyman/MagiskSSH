9e7e90361af77b33ad3a40f9594f1a4da91d24b7
Marc André Wittorf <marc-a@gmx.de>
2022-02-19 11:28:34 +0100

    Update to OpenSSL 3.0.1

--- openssl.mk
+++ openssl.mk
@@ -1,12 +1,13 @@
 $(eval $(call start_package))
-OPENSSL?=openssl-1.1.1k
+OPENSSL?=openssl-3.0.1
 
 PACKAGE:=openssl
 
 ARCHIVE_NAME:=$(OPENSSL).tar.gz
 DOWNLOAD_URL:=https://www.openssl.org/source/$(ARCHIVE_NAME)
+LIBCRYPTO_VERSION:=3
 
-PACKAGE_INSTALLED_FILES:=$(BUILD_DIR)/usr/lib/libcrypto.so.1.1
+PACKAGE_INSTALLED_FILES:=$(BUILD_DIR)/usr/lib/libcrypto.so.$(LIBCRYPTO_VERSION)
 
 define pkg-targets
 $(BUILD_DIR)/$(PACKAGE)/stamp.configured: $(SRC_DIR)/$(PACKAGE)/stamp.prepared
@@ -15,9 +16,9 @@ $(BUILD_DIR)/$(PACKAGE)/stamp.configured: $(SRC_DIR)/$(PACKAGE)/stamp.prepared
 	cd "$(BUILD_DIR)/$(PACKAGE)"; CC=$(CC) LD=$(LD) AS=$(AS) AR=$(AR) ./Configure -DOPENSSL_NO_HEARTBEATS shared "linux-$(OPENSSL_ARCH)" $(CFLAGS) $(LDFLAGS)
 	$(make-configured-stamp)
 
-$(BUILD_DIR)/usr/lib/libcrypto.so.1.1: $(BUILD_DIR)/$(PACKAGE)/stamp.built
+$(BUILD_DIR)/usr/lib/libcrypto.so.$(LIBCRYPTO_VERSION): $(BUILD_DIR)/$(PACKAGE)/stamp.built
 	mkdir -p $(BUILD_DIR)/usr/lib/
-	cp -u "$(BUILD_DIR)/$(PACKAGE)/libcrypto.so.1.1" "$(BUILD_DIR)/usr/lib/"
+	cp -u "$(BUILD_DIR)/$(PACKAGE)/libcrypto.so.$(LIBCRYPTO_VERSION)" "$(BUILD_DIR)/usr/lib/"
 endef
 
 $(eval $(package))
