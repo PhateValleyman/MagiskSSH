7cde3dc69f9f5caa8a9342719895322dd9007a10
Marc André Wittorf <marc-a@gmx.de>
2022-02-19 15:30:08 +0100

    Update magisk installer script

--- magisk_module.mk
+++ magisk_module.mk
@@ -1,6 +1,6 @@
 $(eval $(call start_package)) # not really a package, but we may reset stuff just in case
 
-MAGISK_INSTALLER_VERSION?=v22.1
+MAGISK_INSTALLER_VERSION?=v24.1
 
 .PHONY: module
 module: $(BUILD_DIR)/magisk_ssh-$(VERSION).zip
